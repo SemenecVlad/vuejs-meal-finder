4038741c4456a2f317c2558d36d5fd3945a58aef
Vladislav <admin777@MacBook-Pro-Vladislav.local>
2024-02-07 12:13:48 +0200

    Fixed app input

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,23 +1,19 @@
 <template>
   <input
-    :type="type"
+    v-bind="$props"
     class="rounded border-2 bg-white border-gray-200 w-full mb-3 focus:border-gray-200 focus:outline-none focus:ring-0"
-    :placeholder="placeholder"
-    :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
 
 <script setup>
 import { defineProps } from "vue";
-const { modelValue, type, placeholder } = defineProps({
-  props: {
-    modelValue: String,
-    type: {
-      type: String,
-      default: "text",
-    },
-    placeholder: String,
+const props = defineProps({
+  modelValue: String,
+  type: {
+    type: String,
+    default: "text",
   },
+  placeholder: String,
 });
 </script>
